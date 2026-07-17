bd567432d63c813dab6666216f7a1ba58ed0bf47
Jamison Catts <z5194965@ad.unsw.edu.au>
2026-07-17 21:30:06 +1000

    reverting changes

--- kern/syscall/file.c
+++ kern/syscall/file.c
@@ -121,8 +121,8 @@ int sys_write(int fd, userptr_t buf, size_t size, int32_t *ret_val)
     int result;
 
     char kernel_buf[size];
-    size_t got;
-    result = copyinstr(buf, kernel_buf, size, &got);
+    // size_t got;
+    result = copyin(buf, kernel_buf, size);
 
 
     if (result)
