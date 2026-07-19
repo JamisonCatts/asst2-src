c77ebe55d60a87b897f66d25124979f618ec541c
Jamison Catts <z5194965@ad.unsw.edu.au>
2026-07-19 12:56:39 +1000

    updated declaration in syscall.h

--- kern/include/syscall.h
+++ kern/include/syscall.h
@@ -61,8 +61,8 @@ int sys___time(userptr_t user_seconds, userptr_t user_nanoseconds);
 
 // TODO: add arguments
 int sys_open(userptr_t path, int flags, mode_t mode, int32_t *retval);
-int sys_close();
-int sys_read();
+int sys_close(int fd);
+int sys_read(int fd, userptr_t buf, size_t size, int32_t *return);
 int sys_write(int fd, userptr_t buf, size_t size, int32_t *ret_val);
 
 #endif /* _SYSCALL_H_ */
